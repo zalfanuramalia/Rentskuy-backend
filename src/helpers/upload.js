0a89a527b127344188efd27c17d9ffdfd571504e
zalfanuramalia <zalfafanur@gmail.com>
2022-02-09 21:11:39 +0700

    chore: filter extension

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -7,10 +7,24 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         const _file = file.originalname.split('.');
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, _file[0] + '-' + uniqueSuffix + '.' + _file[1]);
-    }
+        cb(null, _file[0] + '-' + uniqueSuffix + '-' + _file[1]);
+    } 
 });
 
-const upload = multer({storage: storage});
+
+const fileFilter = (req, file, cb) => {
+    const supportedMime = [
+        'image/jpeg', 
+        'image/png', 
+        'image/gif'
+    ];
+    if(!supportedMime.includes(file.mimetype)){
+        cb(new Error('Filetype mismatch!'), false);
+    } else{
+        cb(null, true);
+    }
+};
+
+const upload = multer({storage: storage, fileFilter });
 
 module.exports = upload;
