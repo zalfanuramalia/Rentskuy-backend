480f8a1f8fe9d33680744bc34f405622cbc508a1
zalfanuramalia <zalfafanur@gmail.com>
2022-02-09 22:19:18 +0700

    chore: port

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require ('express');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
 app.use(require('./src/routes'));
+app.use('/uploads', express.static('uploads'));
 
-app.listen(3000, ()=>{
-    console.log('App running on port 3000');
+const {APP_PORT} = process.env;
+
+app.listen(APP_PORT, ()=>{
+    console.log(`App running on port ${APP_PORT}` );
 });
