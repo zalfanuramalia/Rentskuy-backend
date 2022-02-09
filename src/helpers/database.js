7d23aaec6666f2a64cb2447dc2b97a9e09afc2bd
zalfanuramalia <zalfafanur@gmail.com>
2022-02-09 21:12:19 +0700

    chore: prosses env

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
 
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
+
 const con = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'vehicles_rent'
+    host : DB_HOST,
+    user : DB_USER,
+    password : DB_PASSWORD,
+    database : DB_NAME
 });
 
 con.connect();
