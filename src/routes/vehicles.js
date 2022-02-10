24423d43207820df79a5aba613f0b959751abcea
zalfanuramalia <zalfafanur@gmail.com>
2022-02-10 17:59:47 +0700

    chore : verifyUser

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -1,13 +1,13 @@
 const vehicles = require('express').Router();
 
 const {getVehicles, getVehicle, patchVehicle, delVehicle, postVehicle, vehiclesCategory} = require('../controllers/vehicles');
+const {verifyUser} = require('../helpers/auth');
 
-vehicles.get('/', getVehicles);
-vehicles.post('/', postVehicle);
+vehicles.get('/',verifyUser, getVehicles);
+vehicles.post('/',verifyUser, postVehicle);
 vehicles.get('/category/:category_id', vehiclesCategory);
-vehicles.get('/:id', getVehicle);
-vehicles.patch('/:id', patchVehicle);
-// vehicles.patch('/:id', patchVehicleAsync);
-vehicles.delete('/:id', delVehicle);
+vehicles.get('/:id',verifyUser, getVehicle);
+vehicles.patch('/:id',verifyUser, patchVehicle);
+vehicles.delete('/:id',verifyUser, delVehicle);
 
 module.exports = vehicles;
