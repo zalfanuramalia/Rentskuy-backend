642bca551a16e2ac69c2bc8588f930c8dcb781e7
zalfanuramalia <zalfafanur@gmail.com>
2022-02-10 13:29:41 +0700

    chore: post user  with pass enkripsi

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,179 +1,194 @@
 const userModel = require('../models/users');
+const bcrypt = require('bcrypt');
 
 const dataUsers = (req, res) => {
-    let {search, page, limit } = req.query;
-    search = search || '';
-    page = parseInt(page) || 1;
-    limit = parseInt(limit) || 5;
-    const offset = (page - 1) * limit;
-    const data = { search, page, limit, offset };
-    userModel.dataUsers(data, (result) => {
-        userModel.countUsers(data,(count) => {
-            const { total } = count[0];
-            const last = Math.ceil(total/limit);
-            if (result.length > 0){
-                return res.send({
-                    success: true,
-                    message: 'Data Users',
-                    result,
-                    pageInfo: {
-                        prev: page > 1 ? `http://localhost:3000/history?page=${page-1}`: null,
-                        next: page < last ? `http://localhost:3000/history?page=${page+1}`: null,
-                        totalData:total,
-                        currentPage: page,
-                        lastPage: last
-                    }
-                });
-            } else {
-                return res.status(404).send({
-                    success: false,
-                    message: 'Data Users not Found'
-                });
-            }
-        });
+  let {search, page, limit } = req.query;
+  search = search || '';
+  page = parseInt(page) || 1;
+  limit = parseInt(limit) || 5;
+  const offset = (page - 1) * limit;
+  const data = { search, page, limit, offset };
+  if(data.limit < 0 && data.page < 0){
+    return res.status(400).send({
+      success: false,
+      message: 'Page and Limit Must be More Than 0'
     });
+  }
+  if(data.limit < 0){
+    return res.status(400).send({
+      success: false,
+      message: 'Limit Must be More Than 0'
+    });
+  }
+  if(data.page < 0){
+    return res.status(400).send({
+      success: false,
+      message: 'Page Must be More Than 0'
+    });
+  }
+  userModel.dataUsers(data, (result) => {
+    userModel.countUsers(data,(count) => {
+      const { total } = count[0];
+      const last = Math.ceil(total/limit);
+      if (result.length > 0){
+        return res.send({
+          success: true,
+          message: 'Data Users',
+          result,
+          pageInfo: {
+            prev: page > 1 ? `http://localhost:3000/history?page=${page-1}`: null,
+            next: page < last ? `http://localhost:3000/history?page=${page+1}`: null,
+            totalData:total,
+            currentPage: page,
+            lastPage: last
+          }
+        });
+      } else {
+        return res.status(404).send({
+          success: false,
+          message: 'Data Users not Found'
+        });
+      }
+    });
+  });
 };
 
 const dataUser = (req, res) => {
-    const dataID =parseInt(req.params.id);
-    if (!dataID){
-        return res.status(400).send({
-            success: false,
-            message: 'ID must be number!'
-        });
-    }
-    userModel.dataUser(dataID, (result) => {
-        if (result.length > 0){
-            return res.send({
-                success: true,
-                message: 'List Detail User',
-                results: result[0]
-            });
-        } else {
-            return res.status(404).send({
-                success: false,
-                message: 'User Not Found'
-            });
-        }
+  const dataID =parseInt(req.params.id);
+  if (!dataID){
+    return res.status(400).send({
+      success: false,
+      message: 'ID must be number!'
     });
+  }
+  userModel.dataUser(dataID, (result) => {
+    if (result.length > 0){
+      return res.send({
+        success: true,
+        message: 'List Detail User',
+        results: result[0]
+      });
+    } else {
+      return res.status(404).send({
+        success: false,
+        message: 'User Not Found'
+      });
+    }
+  });
 };
 
-const postUser = (req, res) => {
-    const data2 = {
-        id: res.length + 1,
-        name: req.body.name,
-        identity: parseInt(req.body.identity),
-        gender: req.body.gender,
-        email: req.body.email,
-        address: req.body.address,
-        number: req.body.number,
-        birthdate: req.body.birthdate,
-    };
-    if(!data2.identity){
-        return res.status(400).send({
-            success: false,
-            message: 'Identity must be number!'
-        });
-    }
-    userModel.postUser(data2, (result) =>{
-        if (result.affectedRows == 1){
-            return res.send({
-                success: true,
-                message: 'Data User Posted',
-                result: req.body
-            });
-        } else {
-            return res.status(500).send({
-                success: false,
-                message: 'Data not Posted'
-            });
-        }
-    }); 
+const postUser = async (req, res) => {
+  const { name, identity, gender, email, address, number, birthdate, username, password: rawPassword } = req.body;
+  const {id} = res.length + 1;
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(rawPassword, salt);
+  if(!identity){
+    return res.status(400).send({
+      success: false,
+      message: 'Identity must be number!'
+    });
+  }
+  const result = await userModel.postUser({id, name, identity, gender, email, address, number, birthdate, username, password});
+  const get = await userModel.getPostUser();
+  if (result.affectedRows >= 1){
+    return res.send({
+      success: true,
+      message: 'Data User Posted',
+      result: get
+    });
+  } else {
+    return res.status(500).send({
+      success: false,
+      message: 'Data not Posted'
+    });
+  }
 };
 
 const delUser = (req, res) => {
-    const dataID = parseInt(req.params.id);
-    if(!dataID){
-        return res.status(400).send({
+  const dataID = parseInt(req.params.id);
+  if(!dataID){
+    return res.status(400).send({
+      success: false,
+      message: 'ID must be number!'
+    });
+  }    
+  const process = (result) => {
+    if (result.affectedRows == 1){
+      const ress = (result) =>{
+        if(result.length > 0){
+          return res.status(500).send({
             success: false,
-            message: 'ID must be number!'
-        });
-    }    
-    const process = (result) => {
-        if (result.affectedRows == 1){
-            const ress = (result) =>{
-                if(result.length > 0){
-                    return res.status(500).send({
-                        success: false,
-                        message : 'User failed to Delete',
-                        result
-                    });
-                } else {
-                    return res.send({
-                        success: true,
-                        message : 'User was Delete',
-                        result
-                    });
-                }
-            };
-            userModel.delUser( dataID, ress);
+            message : 'User failed to Delete',
+            result
+          });
         } else {
-            return res.status(404).send({
-                success: false,
-                message: 'There is no User with that ID ',
-                result
-            });
+          return res.send({
+            success: true,
+            message : 'User was Delete',
+            result
+          });
         }
-    };
-    userModel.delUser(dataID, process);
+      };
+      userModel.delUser( dataID, ress);
+    } else {
+      return res.status(404).send({
+        success: false,
+        message: 'There is no User with that ID ',
+        result
+      });
+    }
+  };
+  userModel.delUser(dataID, process);
 };
 
 const patchUser = (req, res)=>{
-    const dataID = parseInt(req.params.id);
-    if(!dataID){
-        return res.status(400).send({
-            success: false,
-            message: 'ID must be number!'
-        });
+  const dataID = parseInt(req.params.id);
+  if(!dataID){
+    return res.status(400).send({
+      success: false,
+      message: 'ID must be number!'
+    });
+  }
+  const data = {
+    name: req.body.name,
+    identity: parseInt(req.body.identity),
+    gender: req.body.gender,
+    email: req.body.email,
+    address: req.body.address,
+    number: req.body.number,
+    birthdate: req.body.birthdate,
+    username: req.body.username,
+    password: req.body.password
+  };
+  const em = data.email.indexOf('@');
+  if (em < 1){
+    return res.status(400).send({
+      success: false,
+      message: 'Enter email correctly'
+    });
+  }
+  if(!data.identity){
+    return res.status(400).send({
+      success: false,
+      message: 'Identity must be number!'
+    });
+  }
+  const ress = (result) =>{
+    if (result.affectedRows == 1){
+      return res.send({
+        success: true,
+        message: 'Data User Updated',
+        result: req.body
+      });
+    } else {
+      return res.status(404).send({
+        success: false,
+        message: 'Data Users not Found with that ID'
+      });
     }
-    const data = {
-        name: req.body.name,
-        identity: parseInt(req.body.identity),
-        gender: req.body.gender,
-        email: req.body.email,
-        address: req.body.address,
-        number: req.body.number,
-        birthdate: req.body.birthdate,
-    };
-    const em = data.email.indexOf('@');
-    if (em < 1){
-        return res.status(400).send({
-            success: false,
-            message: 'Enter email correctly'
-        });
-    }
-    if(!data.identity){
-        return res.status(400).send({
-            success: false,
-            message: 'Identity must be number!'
-        });
-    }
-    const ress = (result) =>{
-        if (result.affectedRows == 1){
-            return res.send({
-                success: true,
-                message: 'Data User Updated',
-                result: req.body
-            });
-        } else {
-            return res.status(404).send({
-                success: false,
-                message: 'Data Users not Found with that ID'
-            });
-        }
         
-    };
-    userModel.patchUser(data, dataID, ress);  
+  };
+  userModel.patchUser(data, dataID, ress);  
 };
 
 
--- src/models/users.js
+++ src/models/users.js
@@ -1,44 +1,53 @@
 const db = require ('../helpers/database');
 
 exports.dataUsers = (data, cb) => {
-    db.query(`SELECT * FROM users WHERE name LIKE '%${data.search}%' LIMIT ${data.limit} OFFSET ${data.offset}`, (err, res) => {
-        if (err) throw err;
-        cb(res);
-    });
+  db.query(`SELECT * FROM users WHERE name LIKE '%${data.search}%' LIMIT ${data.limit} OFFSET ${data.offset}`, (err, res) => {
+    if (err) throw err;
+    cb(res);
+  });
 };
 
 exports.countUsers = (data, cb) => {
-    db.query(`SELECT COUNT(*) as total FROM users WHERE name LIKE '%${data.search}%'` , (err, res) => {
-        if (err) throw err;
-        cb(res);
-    });
+  db.query(`SELECT COUNT(*) as total FROM users WHERE name LIKE '%${data.search}%'` , (err, res) => {
+    if (err) throw err;
+    cb(res);
+  });
 };
 
 exports.dataUser = (id, cb) => {
-    db.query('SELECT * FROM users WHERE id = ?',[id], (err, res) => {
-        if (err) throw err;
-        cb(res);
-    });
+  db.query('SELECT * FROM users WHERE id = ?',[id], (err, res) => {
+    if (err) throw err;
+    cb(res);
+  });
 };
 
-exports.postUser = (data2, cb) => {
-    db.query('INSERT INTO users (name, identity, gender, email, address, number, birthdate) VALUES (? , ? , ? , ? , ? , ? , ?)',[data2.name, data2.identity, data2.gender, data2.email, data2.address, data2.number, data2.birthdate], (error, res) => {
-        if (error) throw error;
-        cb(res);
-    });
-};
+exports.postUser = (data2) => new Promise ((resolve, reject) => {
+  db.query(`INSERT INTO users (name, identity, gender, email, address, number, birthdate, username, password) VALUES 
+  ('${data2.name}', '${data2.identity}', '${data2.gender}', '${data2.email}', '${data2.address}', '${data2.number}', '${data2.birthdate}', '${data2.username}', '${data2.password}')`,
+  (err, res) => {
+    if (err) reject (err);
+    resolve(res);
+  });
+});
+
+exports.getPostUser = () => new Promise ((resolve, reject) => {
+  db.query('SELECT * FROM users ORDER BY id DESC LIMIT 1', (err, res) => {
+    if (err) reject (err);
+    resolve(res);
+  });
+});
 
 exports.delUser = (id, cb) => {
-    db.query('DELETE FROM users WHERE id = ?',[id], (error, res) => {
-        if (error) throw error;
-        cb(res);
-    });
+  db.query('DELETE FROM users WHERE id = ?',[id], (error, res) => {
+    if (error) throw error;
+    cb(res);
+  });
 };
 
 exports.patchUser = (data, id, cb) => {
-    db.query('UPDATE users SET name = ? , identity = ? , gender = ? , email =  ? , address = ? , number = ? , birthdate = ? WHERE id = ?', 
-        [data.name, data.identity, data.gender, data.email, data.address, data.number, data.birthdate, id], (error, res) => {
-            if (error) throw error;
-            cb(res);
-        });
+  db.query('UPDATE users SET name = ? , identity = ? , gender = ? , email =  ? , address = ? , number = ? , birthdate = ? WHERE id = ?', 
+    [data.name, data.identity, data.gender, data.email, data.address, data.number, data.birthdate, id], (error, res) => {
+      if (error) throw error;
+      cb(res);
+    });
 };
