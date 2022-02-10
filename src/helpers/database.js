191f02281b6386e8fddebc58c00d9ee7e49db5aa
zalfanuramalia <zalfafanur@gmail.com>
2022-02-10 17:57:59 +0700

    chore: change from eslint

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
 
 const con = mysql.createConnection({
-    host : DB_HOST,
-    user : DB_USER,
-    password : DB_PASSWORD,
-    database : DB_NAME
+  host : DB_HOST,
+  user : DB_USER,
+  password : DB_PASSWORD,
+  database : DB_NAME
 });
 
 con.connect();
