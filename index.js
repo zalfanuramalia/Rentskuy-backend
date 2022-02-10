7652a30a6f6645922e9c1b80da8fa95e12e72df2
zalfanuramalia <zalfafanur@gmail.com>
2022-02-10 17:55:39 +0700

    chore: edited port

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use('/uploads', express.static('uploads'));
 const {APP_PORT} = process.env;
 
 app.listen(APP_PORT, ()=>{
-    console.log(`App running on port ${APP_PORT}` );
+  console.log(`App running on port ${APP_PORT}` );
 });
