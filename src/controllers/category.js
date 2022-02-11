a48382584b3bbade1b8c3555e01b8d3349149986
zalfanuramalia <zalfafanur@gmail.com>
2022-02-11 16:33:06 +0700

    fix: influence from eslint

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,107 +1,107 @@
 const categoryModel = require('../models/category');
 
 const categories = (req, res)=>{
-    categoryModel.categories((result) =>{
-        if (result.length > 0){
-            return res.send({
-                success: true,
-                message: 'Vehicles Categories',
-                result,                    
-            });
-        } else {
-            return res.status(404).send({
-                success: false,
-                message: 'Vehicle Categiroes Not Found',
-            });
-        }    
-    });
+  categoryModel.categories((result) =>{
+    if (result.length > 0){
+      return res.send({
+        success: true,
+        message: 'Vehicles Categories',
+        result,                    
+      });
+    } else {
+      return res.status(404).send({
+        success: false,
+        message: 'Vehicle Categiroes Not Found',
+      });
+    }    
+  });
 };
 
 const postCategories = (req, res) => {
-    const data = {
-        id: res.length + 1,
-        name: req.body.name
-    };
-    categoryModel.postCategories(data, (result) =>{
-        if (result.affectedRows == 1){
-            return res.send({
-                success: true,
-                message: 'Data Posted',
-                result
-            });
-        } else {
-            return res.status(404).send({
-                success: false,
-                message: 'Data not Posted'
-            });
-        }
+  const data = {
+    id: res.length + 1,
+    name: req.body.name
+  };
+  categoryModel.postCategories(data, (result) =>{
+    if (result.affectedRows == 1){
+      return res.send({
+        success: true,
+        message: 'Data Posted',
+        result
+      });
+    } else {
+      return res.status(404).send({
+        success: false,
+        message: 'Data not Posted'
+      });
+    }
       
-    }); 
+  }); 
 };
 
 const delCategories = (req, res) => {
-    const dataID = parseInt(req.params.id);
-    if (!dataID){
-        return res.status(400).send({
+  const dataID = parseInt(req.params.id);
+  if (!dataID){
+    return res.status(400).send({
+      success: false,
+      message: 'ID must be number!'
+    });
+  }
+  const process = (result) => {
+    if (result.affectedRows == 1){
+      const ress = (result) =>{
+        if(result.length > 0){
+          return res.status(500).send({
             success: false,
-            message: 'ID must be number!'
-        });
-    }
-    const process = (result) => {
-        if (result.affectedRows == 1){
-            const ress = (result) =>{
-                if(result.length > 0){
-                    return res.status(500).send({
-                        success: false,
-                        message : 'Category Failed to Delete',
-                        result
-                    });
-                } else {
-                    return res.send({
-                        success: true,
-                        message : 'Category was Delete',
-                        result
-                    });
-                }
-            };
-            categoryModel.delCategories( dataID, ress);
+            message : 'Category Failed to Delete',
+            result
+          });
         } else {
-            return res.status(404).send({
-                success: false,
-                message: 'There is no Category with that ID ',
-            });
+          return res.send({
+            success: true,
+            message : 'Category was Delete',
+            result
+          });
         }
-    };
-    categoryModel.delCategories(dataID, process);
+      };
+      categoryModel.delCategories( dataID, ress);
+    } else {
+      return res.status(404).send({
+        success: false,
+        message: 'There is no Category with that ID ',
+      });
+    }
+  };
+  categoryModel.delCategories(dataID, process);
 };
 
 const patchCategory = (req, res)=>{
-    const dataID = parseInt(req.params.id);
-    if (!dataID){
-        return res.status(400).send({
-            success: false,
-            message: 'ID must be number!'
-        });
+  const dataID = parseInt(req.params.id);
+  if (!dataID){
+    return res.status(400).send({
+      success: false,
+      message: 'ID must be number!'
+    });
+  }
+  const data = {
+    name: req.body.name,
+  };
+  const ress = (result) =>{
+    if (result.affectedRows == 1){
+      return res.send({
+        success: true,
+        message: 'Data Category Updated',
+        result: req.body
+      });
+    } else {
+      return res.status(404).send({
+        success: false,
+        message: 'Data Category not Found'
+      });
     }
-    const data = {
-        name: req.body.name,
-    };
-    const ress = (result) =>{
-        if (result.affectedRows == 1){
-            return res.send({
-                success: true,
-                message: 'Data Category Updated',
-                result: req.body
-            });
-        } else {
-            return res.status(404).send({
-                success: false,
-                message: 'Data Category not Found'
-            });
-        }
       
-    };
-    categoryModel.patchCategory(data, dataID, ress);  
+  };
+  categoryModel.patchCategory(data, dataID, ress);  
 };
 
 module.exports = {categories, postCategories, delCategories, patchCategory};
--- src/models/category.js
+++ src/models/category.js
@@ -1,29 +1,29 @@
 const db = require ('../helpers/database');
 
 exports.categories = (cb) => {
-    db.query('SELECT * FROM category', (err, res) => {
-        if (err) throw err;
-        cb(res);
-    });
+  db.query('SELECT * FROM category', (err, res) => {
+    if (err) throw err;
+    cb(res);
+  });
 };
 
 exports.postCategories = (data, cb) => {
-    db.query('INSERT INTO category (name) VALUES (?)',[data.name], (err, res) => {
-        if (err) throw err;
-        cb(res);
-    });
+  db.query('INSERT INTO category (name) VALUES (?)',[data.name], (err, res) => {
+    if (err) throw err;
+    cb(res);
+  });
 };
 
 exports.delCategories = (id, cb) => {
-    db.query('DELETE FROM category WHERE id = ?',[id], (error, res) => {
-        if (error) throw error;
-        cb(res);
-    });
+  db.query('DELETE FROM category WHERE id = ?',[id], (error, res) => {
+    if (error) throw error;
+    cb(res);
+  });
 };
 
 exports.patchCategory = (data, id, cb) => {
-    db.query('UPDATE category SET name = ? WHERE id = ?', [data.name, id], (error, res) => {
-        if (error) throw error;
-        cb(res);
-    });
+  db.query('UPDATE category SET name = ? WHERE id = ?', [data.name, id], (error, res) => {
+    if (error) throw error;
+    cb(res);
+  });
 };
