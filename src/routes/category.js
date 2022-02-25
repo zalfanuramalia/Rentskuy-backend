760ab7da6a4b86dd2856710bdca96caa94b759e8
zalfanuramalia <zalfafanur@gmail.com>
2022-02-25 22:53:41 +0700

    chore: add cors

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,10 +1,11 @@
 const category = require('express').Router();
+const cors = require('cors');
 
 const {categories, postCategories, delCategories, patchCategory} = require('../controllers/category');
-const {verifyUser} = require('../helpers/auth');
+// const {verifyUser} = require('../helpers/auth');
 
-category.get('/', verifyUser, categories);
-category.post('/', postCategories);
+category.get('/', cors(), categories);
+category.post('/',cors(), postCategories);
 category.delete('/:id', delCategories);
 category.patch('/:id', patchCategory);
 
