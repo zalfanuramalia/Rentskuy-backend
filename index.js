4c8ab799946cf9375c39d64e67deb582d1234edb
zalfanuramalia <zalfafanur@gmail.com>
2022-03-05 15:49:01 +0700

    chore: add cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require ('express');
 require('dotenv').config();
 
 const app = express();
+const cors = require('cors');
 
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 app.use(require('./src/routes'));
 app.use('/uploads', express.static('uploads'));
