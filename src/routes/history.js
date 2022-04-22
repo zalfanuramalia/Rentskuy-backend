e1229432ef51495f5f0a9e019e914f563278f487
zalfanuramalia <zalfafanur@gmail.com>
2022-04-22 12:28:13 +0700

    Deploy to Heroku!

--- src/routes/history.js
+++ src/routes/history.js
@@ -8,7 +8,7 @@ history.get('/vehicles/createdAt', cors(),  popularBasedonMonth);
 history.get('/vehicles', cors(), popularVehicles);
 history.get('/', cors(), dataHistory);
 history.get('/:id', cors(), detailHistory);
-history.get('/users/:id',  cors(),  detailHistoryUser);
+history.get('/users/:id', userVerify, cors(),  detailHistoryUser);
 history.post('/', userVerify, cors(), postHistory);
 history.delete('/:id', userVerify, cors(), delHistory);
 history.patch('/:id', userVerify, cors(), patchHistory);
